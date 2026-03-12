fcb9eece74564b8e435f88f2cc20bc13dc6c30a7
Omar <omar.garcia@morsa.com.mx>
2026-03-12 12:30:01 -0600

    Añadiendo el PICKER y OPC

--- components/Formulario.js
+++ components/Formulario.js
@@ -1,3 +1,4 @@
+import { Picker } from '@react-native-picker/picker';
 import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 const Formulario = () => {
@@ -6,6 +7,19 @@ const Formulario = () => {
       <View style={styles.formulario}>
         <View>
           <TextInput placeholder="Ciudad" placeholderTextColor="#666" />
+        </View>
+
+        <View>
+          <Picker>
+            <Picker.Item label="-- Selecciona un país --" value="" />
+            <Picker.Item label="Estados Unidos" value="US" />
+            <Picker.Item label="México" value="MX" />
+            <Picker.Item label="Argentina" value="AR" />
+            <Picker.Item label="Colombia" value="CO" />
+            <Picker.Item label="Costa Rica" value="CR" />
+            <Picker.Item label="España" value="ES" />
+            <Picker.Item label="Perú" value="PE" />
+          </Picker>
         </View>
       </View>
     </>
