4f54a059ab2557a47dc7cc8c511661eafe40e8c1
Omar <omar.garcia@morsa.com.mx>
2026-03-13 14:31:00 -0600

    Cambiando el COLOR de FONDO segun el RESULTADO y FINALIZANDO APP

--- App.js
+++ App.js
@@ -18,6 +18,7 @@ const App = () => {
   const [consultar, guardarConsultar] = useState(false);
   const [resultado, guardarResultado] = useState({});
   const { ciudad, pais } = busqueda;
+  const [bgColor, guardarBgColor] = useState('rgb(71, 149, 212)');
 
   useEffect(() => {
     const consultarClima = async () => {
@@ -35,6 +36,19 @@ const App = () => {
 
           guardarResultado(resultado);
           guardarConsultar(false);
+
+          // Modifica los colores de fondo basado en la temperatura
+          const kelvin = 273.15;
+          const { main } = resultado;
+          const actual = main.temp - kelvin;
+
+          if (actual < 10) {
+            guardarBgColor('rgb(105, 108, 149)');
+          } else if (actual >= 10 && actual < 25) {
+            guardarBgColor('rgb(71, 149, 212)');
+          } else {
+            guardarBgColor('rgb(178, 28, 28)');
+          }
         } catch (error) {
           mostrarAlerta();
         }
@@ -55,10 +69,14 @@ const App = () => {
     Keyboard.dismiss();
   };
 
+  const bgColorApp = {
+    backgroundColor: bgColor,
+  };
+
   return (
     <>
       <TouchableWithoutFeedback onPress={() => ocultarTeclado()}>
-        <View style={styles.app}>
+        <View style={[styles.app, bgColorApp]}>
           <View style={styles.contenido}>
             <Clima resultado={resultado} />
 
@@ -76,7 +94,7 @@ const App = () => {
 
 const styles = StyleSheet.create({
   app: {
-    backgroundColor: 'rgb(71, 149, 212)',
+    // backgroundColor: 'rgb(71, 149, 212)',
     flex: 1,
     justifyContent: 'center',
   },
